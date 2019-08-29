a2a24e4db7935b152e2ab8bc35921de5c51eec89
Tsukirii <liz.m.e.91@gmail.com>
2019-08-29 10:24:48 -0500

    Create Cambiar botones a texto.js

---
+++ cherrypop/JavaScript/Subforos&Temas/Cambiar botones a texto.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function() {
+  $('.i_reply').replaceWith('<div class="cranberry-b">responder</div>');
+  $('.i_post').replaceWith('<div class="cranberry-a">nuevo tema</div>');
+  $('.i_icon_quote').replaceWith('<div class="salak"><em class="sf sf-quotation-mark"></em> Citar</div>');
+  $('.i_icon_edit').replaceWith('<div class="salak"><em class="sf sf-pencil"></em> Editar</div>');
+  $('.i_icon_delete').replaceWith('<div class="salak"><em class="sf sf-cross"></em> Borrar</div>');
+  $('.i_icon_ip').replaceWith('<div class="salak"><em class="sf sf-location"></em> Ver IP</div>');
+});
